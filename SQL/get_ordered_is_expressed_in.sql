7db355eb9eed4f0de5e6c2e8d54efb8dd59124d0
tmendes <tarcisio.mendes@sib.swiss>
2021-11-11 14:18:57 +0100

    added human

--- SQL/get_ordered_is_expressed_in.sql
+++ SQL/get_ordered_is_expressed_in.sql
@@ -3,5 +3,5 @@ SELECT g.geneId as gene_id, REPLACE(SUBSTRING_INDEX(cond.anatEntityId,'UBERON:',
 JOIN gene as g ON g.bgeeGeneId = ge.bgeeGeneId 
 JOIN globalCond as cond on cond.globalConditionId = ge.globalConditionId  
 JOIN temp_wikidata_uberon_ids as wd on wd.uberon_anatomical_id = cond.anatEntityId
-where ge.callType='EXPRESSED'   and cond.speciesId in (10090)  group by gene_id, uberon_id ) as c
+where ge.callType='EXPRESSED'   and cond.speciesId in (9606, 10090)  group by gene_id, uberon_id ) as c
 group by gene_id, uberon_id order by gene_id, max(score) desc limit 2500
